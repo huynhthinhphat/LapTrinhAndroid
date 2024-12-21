be3ddee5175e1a3471b42858f93f9e2208768115
huynhthinhphat <cnttk44dhuynhthinhphat0690@gmail.com>
2024-12-21 22:19:19 +0700

    Xong Meo Thi

---
+++ app/src/main/java/com/example/gplxhanga/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.gplxhanga;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Button btnMeoThi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+        setUpBtnMeoThi();
+    }
+
+    private void init(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnMeoThi = findViewById(R.id.btn_meo_thi);
+    }
+
+    private void setUpBtnMeoThi(){
+        btnMeoThi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FifthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
